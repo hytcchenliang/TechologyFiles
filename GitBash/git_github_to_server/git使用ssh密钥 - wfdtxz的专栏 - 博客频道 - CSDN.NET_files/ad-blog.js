db25d902c5e7f826c953f91975be78d7377ad1f2
unknown <cl211111003@163.com>
2015-06-09 10:25:24 +0800

    the first time to commit this repos

---
+++ GitBash/git_github_to_server/git使用ssh密钥 - wfdtxz的专栏 - 博客频道 - CSDN.NET_files/ad-blog.js
@@ -0,0 +1,32 @@
+var ad_type = 'ms1293';
+var ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+
+ad_type = 'ms1297_2';
+ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+
+document.writeln(ad_sc);
+
+
+ad_type = 'ms1300_3';
+ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+
+document.writeln(ad_sc);
+/*
+ad_type = 'ms1269_4';
+ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+
+document.writeln(ad_sc);
+*/
